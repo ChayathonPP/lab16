f681e5f63d946903fd606b3136881d4015c53f39
Chayathon <chayathon_p@cmu.ac.th>
2023-02-07 15:36:50 +0700

    lab16_4 COMPLETE

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -17,3 +17,27 @@ int main(){
 	
 	return 0;
 }
+void shuffle(int &a, int &b, int &c, int &d)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        int randomIndex = rand() % 4;
+        switch (randomIndex)
+        {
+            case 0:
+                std::swap(a, b);
+                break;
+            case 1:
+                std::swap(a, c);
+                break;
+            case 2:
+                std::swap(a, d);
+                break;
+            case 3:
+                std::swap(b, c);
+                break;
+            default:
+                break;
+        }
+    }
+}
